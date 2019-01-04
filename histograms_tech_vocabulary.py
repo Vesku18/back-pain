56cf9a7c04046dee248026f8152fdf353e62a703
smurtova <45288094+smurtova@users.noreply.github.com>
2019-01-04 19:16:36 +0200

    Update histograms_tech_vocabulary.py

--- histograms_tech_vocabulary.py
+++ histograms_tech_vocabulary.py
@@ -6,31 +6,35 @@ import seaborn as sns
 #def hist():
 df = pd.read_excel('technical_vocabulary.xlsx', sheet_name = 'vocabulary-1')
 
-
 def Disease():
     fig = plt.figure()
-    ax=sns.countplot(x='disease_class', data=df, color='C0')
-    plt.xticks(x='disease_class', rotation='vertical')
-    plt.title('Disease distribution',fontsize=16)
+    ay=sns.countplot(y='disease_class', data=df, color='C0', order = df['disease_class'].value_counts().index)
+    ay.set_ylabel('')
+    ay.set_xlabel('Count by classes')
+    plt.title('Disease distribution')
     plt.savefig("Disease_distribution.png")
 
-def  Symptoms():
+def Symptoms():
     fig = plt.figure()
-    ax=sns.countplot(x='symptom_class', data=df, color='C0')
-    plt.xticks(x='symptom_class', rotation='vertical')
-    plt.title('Symptoms',fontsize=16)
+    ay=sns.countplot(y='symptom_class', data=df, color='C0', order = df['symptom_class'].value_counts().index)
+    plt.title('Symptoms') 
+    ay.set_ylabel('')
+    ay.set_xlabel('Count by classes')
     plt.savefig("Symptoms_count.png")
 
 def Therapy():
     fig = plt.figure()
-    ax=sns.countplot(x='therapy_class', data=df, color='C0')
-    plt.xticks(x='therapy_class', rotation='vertical')
-    plt.title('Therapy',fontsize=16)
+    ay=sns.countplot(y='therapy_class', data=df, color='C0', order = df['therapy_class'].value_counts().index)
+    plt.title('Therapy')    
+    ay.set_ylabel('')
+    ay.set_xlabel('Count by classes')
     plt.savefig("Therapy_count.png")
 
 def life_style():
     fig = plt.figure()
-    ax=sns.countplot(x='life style_class', data=df, color='C0')
-    plt.xticks(x='life style_class', rotation='vertical')
-    plt.title('Life Style',fontsize=16)
+    ay=sns.countplot(y='life style_class', data=df, color='C0', order = df['life style_class'].value_counts().index)
+    plt.xticks(x='life_style_class', rotation='vertical')
+    plt.title('Life Style')
+    ay.set_ylabel('')
+    ay.set_xlabel('Count by classes')
     plt.savefig("Life_Style_count.png")
